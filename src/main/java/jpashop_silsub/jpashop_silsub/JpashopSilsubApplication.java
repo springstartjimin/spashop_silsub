10aae31d677895ea34a19d6ff069e69cb9144bd3
Song <misosjm@naver.com>
2022-10-18 20:47:23 +0900

    init

---
+++ src/main/java/jpashop_silsub/jpashop_silsub/JpashopSilsubApplication.java
@@ -0,0 +1,13 @@
+package jpashop_silsub.jpashop_silsub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopSilsubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopSilsubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpashop_silsub/jpashop_silsub/JpashopSilsubApplicationTests.java
@@ -0,0 +1,13 @@
+package jpashop_silsub.jpashop_silsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopSilsubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
